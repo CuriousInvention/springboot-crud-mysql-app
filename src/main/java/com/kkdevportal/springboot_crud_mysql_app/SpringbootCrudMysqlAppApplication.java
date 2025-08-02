0b661e755228e955e52765dabda7cb7d0df9e8fb
kaushal kishor <stardust@niit.localdomain>
2025-08-02 11:13:38 +0530

    maven project

---
+++ src/main/java/com/kkdevportal/springboot_crud_mysql_app/SpringbootCrudMysqlAppApplication.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.springboot_crud_mysql_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudMysqlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudMysqlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkdevportal/springboot_crud_mysql_app/SpringbootCrudMysqlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdevportal.springboot_crud_mysql_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudMysqlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
